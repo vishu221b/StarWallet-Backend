be14188af0b5df52f4446dc976f0f67e4fec714b
vishu221b <vishal.dogra738@gmail.com>
2021-10-13 04:45:09 +0530

    Transaction update to latest in Wallet Recharge scheduler update

--- src/main/java/com/StarWallet/StarWallet/scheduler/WalletRechargeScheduler.java
+++ src/main/java/com/StarWallet/StarWallet/scheduler/WalletRechargeScheduler.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @Slf4j
 @Component
@@ -34,11 +35,13 @@ public class WalletRechargeScheduler {
             log.info(String.format("Updating %s wallet recharges.", walletRecharges.size()));
             walletRecharges.forEach(wr -> {
                 Collection<Transaction> transactions = transactionRepository.findByParentTransactionId(wr.getTransaction());
-                if(transactions.size() > 0 &&
-                        transactions
-                                .stream()
-                                .anyMatch(x -> x.getTransactionStatus().equals(TransactionStatus.COMPLETED))){
+                Optional<Transaction> txn = transactions
+                        .stream()
+                        .filter(x -> x.getTransactionStatus().equals(TransactionStatus.COMPLETED))
+                        .findFirst();
+                if(transactions.size() > 0 && txn.isPresent()){
                     wr.setRechargeStatus(RechargeStatus.SUCCESSFUL);
+                    wr.setTransaction(txn.get());
                 }
             });
             log.info(String.format("Updated %s wallet recharges.", walletRecharges.size()));
